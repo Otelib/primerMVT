f391727de5fd1551b048142123aeddee2ddd1b08
Marcelo De Oto <otelib@hotmail.com>
2022-09-30 18:46:34 -0300

    agregue templates

--- primerMVT/settings.py
+++ primerMVT/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'primerMVT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [f"{BASE_DIR}/primerMVT/Familia/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

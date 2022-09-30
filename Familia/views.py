709f5519450dba99e80b979889edb7d40c8db266
Marcelo De Oto <otelib@hotmail.com>
2022-09-30 19:32:34 -0300

    agrego prueba templates

--- Familia/views.py
+++ Familia/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
+
 def show_home(request):
-    return HttpResponse ("Hola Mundo!")
+    return render(request, "Familia/familiar1.html")

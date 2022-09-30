ab97382bf97bd5f19d048440df1a7b3cc4d69478
Marcelo De Oto <otelib@hotmail.com>
2022-09-30 20:13:43 -0300

    listo primera entrega

--- Familia/models.py
+++ Familia/models.py
@@ -1,11 +1,14 @@
+from dataclasses import dataclass
+from datetime import date
 import os
 from django.db import models
 from django.forms import DateField
+from datetime import datetime
 
 # Create your models here.
 
 
 class Familiar(models.Model):
-    Nombre = models.CharField(max_length=30)
-    Apellido = models.CharField(max_length=30)
-    Edad = models.IntegerField()
+    nombre = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    nacimiento = models.DateField(default=datetime.now)
--- Familia/views.py
+++ Familia/views.py
@@ -1,6 +1,17 @@
+import email
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from Familia.models import Familiar
 # Create your views here.
 
 def show_home(request):
-    return render(request, "Familia/familiar1.html")
+    familiar1 = Familiar(nombre = "Marcelo", edad = 37,  nacimiento = "1985-03-26")
+    familiar2 = Familiar(nombre = "Dario", edad = 39,  nacimiento = "1983-05-13")
+    familiar3 = Familiar(nombre = "Juliana", edad = 32,  nacimiento = "1991-09-20")
+    familiar1.save()
+    familiar2.save()
+    familiar3.save()
+    context = {"familiar1": familiar1, "familiar2": familiar2, "familiar3": familiar3}
+    return render(request, "Familia/familiar1.html", context)
+ 
